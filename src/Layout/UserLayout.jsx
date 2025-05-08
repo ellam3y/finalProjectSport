5acb88d59532898d9b89d2a2903a948139c8117c
ellam3y <midoellamaey@gmail.com>
2025-05-08 16:36:56 +0200

    link4

--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/Header/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Global/Footer/Footer";
 import SearchBar from "../components/Global/SearchBar/SearchBar";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 export default function UserLayout() {
   const [showScrollButton, setShowScrollButton] = React.useState(false);
@@ -21,24 +22,13 @@ export default function UserLayout() {
   }, []);
 
   return (
-    <div style={{ position: "relative" }}>
+    <div className="relative">
       {showScrollButton && (
         <button
-          style={{
-            position: "fixed",
-            bottom: "20px",
-            right: "5px",
-            padding: "10px 15px",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-            zIndex: 1000,
-          }}
+          className="fixed bottom-5 right-2 p-3 bg-blue-950 text-white rounded-md shadow-lg hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-400 z-50"
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
-          ↑
+          <AiOutlineArrowUp style={{ fontSize: "1.5rem" }} />
         </button>
       )}
       <NavBar />
